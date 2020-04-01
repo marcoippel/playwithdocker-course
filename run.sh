3b0d58253f8b284e2999a2f0edce8941ae6fc8f1
Marco Ippel <marco@ippelsoft.nl>
2020-04-01 20:39:13 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl https://raw.githubusercontent.com/marcoippel/playwithdocker-course/master/pwd-docker-compose.yml --output docker-compose.yaml
-docker-compose up -d
+docker-compose up \-d
